c6dc847157068c521597d8cb5feb07885c839988
Andre Rousselet <andre098@hotmail.com>
2023-09-30 12:13:14 -0300

    feat(app/env): register fastify/jwt plugin with secret key env

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,14 +4,18 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
 
+/**
+ * para derrubar app caso alguma variável não seja validada
+ */
 if (_env.success === false) {
   console.error("❌ Invalid environment variables.", _env.error.format());
 
-  throw new Error("Invalid environment variables."); // para derrubar app caso alguma variável não seja validada
+  throw new Error("Invalid environment variables.");
 }
 
 export const env = _env.data;
