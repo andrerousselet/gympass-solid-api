5bcffed1e2809a756368c7ce219730bfa9ee1317
Andre Rousselet <andre098@hotmail.com>
2023-10-03 16:07:48 -0300

    test(prisma-test-environment): setup prisma test environment config
    isolated schemas for each test suite and drop after

--- prisma/vitest-environment-prisma/prisma-test-environment.js
+++ prisma/vitest-environment-prisma/prisma-test-environment.js
@@ -1,14 +1,33 @@
-// import { Environment } from "vitest";
+import "dotenv/config";
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("You must set the DATABASE_URL environment variable");
+  }
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+  return String(url);
+}
 
 export default {
   name: "prisma",
   transformMode: "ssr",
   async setup() {
-    console.log("Setup");
-
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+    process.env.DATABASE_URL = databaseURL;
+    execSync("npx prisma migrate deploy");
     return {
       async teardown() {
-        console.log("Teardown");
+        await prisma.$executeRawUnsafe(
+          `drop schema if exists "${schema}" cascade`,
+        );
+        await prisma.$disconnect();
       },
     };
   },
