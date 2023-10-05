b18fd24301d4591792fcaad298dcdd0c591b9674
Andre Rousselet <andre098@hotmail.com>
2023-10-05 18:42:46 -0300

    test(check-ins-controllers): add tests for checkins creation, history, metrics and validation

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { createAndAuthenticateUser } from "@/utils/test/create-and-authenticate-user";
+import { prisma } from "@/lib/prisma";
+
+describe("Create check-in controller (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to get create a check-in", async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: "The Gym",
+        latitude: -22.9438099,
+        longitude: -43.1956681,
+      },
+    });
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        latitude: -22.9438099,
+        longitude: -43.1956681,
+      });
+
+    expect(response.statusCode).toBe(201);
+  });
+});
